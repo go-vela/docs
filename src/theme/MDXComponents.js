ef1da1f583cbe9fd1ec5aeea53137dec74ddf032
davidvader <davelvader@gmail.com>
2024-12-16 09:43:39 -0600

    feat: mdx icons and color tweaks

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas);
+
+export default {
+  ...MDXComponents,
+  FAIcon: FontAwesomeIcon,
+};
+
