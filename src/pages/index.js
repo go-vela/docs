14539dd2af608dd6b54bc8c3db95deba786c6389
davidvader <davelvader@gmail.com>
2024-12-19 14:30:23 -0600

    fix: change homepage title prefix to Docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Docs`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
