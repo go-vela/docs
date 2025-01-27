83f9ba3494af45e10fad42d112a792a31cbd2cbb
Kell Sanders <Kell.Sanders@target.com>
2025-01-27 10:19:35 -0600

    tweaking config url further

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
     ...remoteContentVelaPlugins,
   ],
 
-  // GitHub pages deployment config.
+  // GitHub pages deployment config...
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'go-vela',
   projectName: 'docs',
