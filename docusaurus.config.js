6bf16d81ecc618704ee681eae4b25ac1d48b4780
Kell Sanders <Kell.Sanders@target.com>
2025-01-27 10:27:26 -0600

    added trailingslash to docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   organizationName: 'go-vela',
   projectName: 'docs',
   deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
