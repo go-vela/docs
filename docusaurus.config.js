5e46a6293a2e6ee6c76bc2d9acd7eec0c98cf1b9
Kell Sanders <Kell.Sanders@target.com>
2025-01-27 07:52:35 -0600

    tweaked edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,10 +10,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://pages.git.target.com',
+  url: 'https://go-vela.github.io/docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-v2/',
+  baseUrl: '/',
   plugins: [
     require.resolve('docusaurus-lunr-search'),
     ...remoteContentVelaPlugins,
@@ -21,8 +21,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'actions-playground',
-  projectName: 'vela-prototype',
+  organizationName: 'go-vela',
+  projectName: 'docs-v2',
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
